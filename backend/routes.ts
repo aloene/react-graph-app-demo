eda9fb93121ef11303da0c6b153121efd0221df4
GERARD Alexandre <alexandre.gerard@neos-sdi.com>
2020-11-16 23:49:10 +0100

    ADD: Roles check

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,14 +1,24 @@
 import { CustomersController } from './controllers/customers-controller';
-import { Application as ExpressApp } from 'express';
-import { UsersController } from "./controllers/users-controller";
+import { Application as ExpressApp, Request, Response, NextFunction } from 'express';
+import { UsersController } from './controllers/users-controller';
 
 export class Routes {
   public usersController: UsersController = new UsersController();
   public customersController: CustomersController = new CustomersController();
 
   public routes(app: ExpressApp): void {
-    app.route("/users/current").get(this.usersController.current);
+    app.route("/users/current").get(this.isAuthorized('Reader', 'Admin'), this.usersController.current);
+    app.route("/customers").get(this.isAuthorized('Admin'), this.customersController.all);
+  }
 
-    app.route("/customers").get(this.customersController.all);
+  private isAuthorized(... roles: string[]) {
+    return (req: Request<any, any, any, any>, res: Response<any>, next: NextFunction) => {
+      if (roles.indexOf(req.user['role']) > -1) {
+        next();
+      }
+      else {
+        res.status(403).send('Not authorized');
+      }
+    };
   }
 }
